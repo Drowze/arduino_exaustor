18bd4da16c9e92a3e0c1ad8235e2dd2832a0a3cc
Rafael G <gibim6@gmail.com>
2015-05-06 23:44:15 -0300

    Update funciona.ino

--- funciona.ino
+++ funciona.ino
@@ -1,5 +1,5 @@
-/* Exemplo do Sensor de Chuva
-Equipe LdG
+/* 
+Simulador de exaustor de cozinha
 */     
 #define SAIDA_CHUVA 2
 #define SAIDA_LDR 8
@@ -10,7 +10,7 @@ Equipe LdG
 unsigned long timer;
 
 int ldr; //Variável para a leitura do LDR
-int cont; //Variável utilizada para armazenar os valores lidos pelo sensor
+int cont; //Variável utilizada para armazenar os valores temporários lidos pelo LDR
 int i; //Variável para contagem
 
 void setup(){
@@ -18,22 +18,21 @@ void setup(){
   
   pinMode(ENT_CHUVA, INPUT);
 	
-	pinMode(SAIDA_CHUVA, OUTPUT);
+  pinMode(SAIDA_CHUVA, OUTPUT);
   pinMode(SAIDA_LDR, OUTPUT);
 }
 
 void loop(){
   if (digitalRead(ENT_CHUVA) == 0){
     timer = millis(); //renovo a variavel timer
-    digitalWrite(SAIDA_CHUVA, HIGH);
+    digitalWrite(SAIDA_CHUVA, HIGH); //ligo a saída do sensor de chuva
     
     Serial.print(digitalRead(ENT_CHUVA));
     Serial.println(" Saida do sensor de chuva ligada");
-    //delay(2000);
   }
   else{
     if(millis() - timer >= 10000UL){ //checo se já fazem 10 segundos desde a ultima mudança de timer
-      digitalWrite(SAIDA_CHUVA, LOW); // deixo a ventoinha ligada por 10 segundos (substitui a funcao delay)
+      digitalWrite(SAIDA_CHUVA, LOW); // se fizer mais de 10 segundos, desligo a saída
       Serial.println(" saida do sensor de chuva desligada");
     }
     Serial.print(digitalRead(ENT_CHUVA));
@@ -42,14 +41,14 @@ void loop(){
   
   ldr=0; //Zera a variável LDR
   for(i=0;i<=10;i++){ //Faz 10 vezes a leitura do sensor
-		cont=analogRead(A0); //Lê o valor do sensor (LDR ligado ao pino A0) e guarda na variável LDR
-		ldr += cont; //Armazenamento na varíavel LDR o valor lido + o valor anterior
-		delay(10); //Delay se 10 milissegundos
+    cont=analogRead(A0); //Lê o valor do sensor (LDR ligado ao pino A0) e guarda na variável LDR
+    ldr += cont; //Armazenamento na varíavel LDR o valor lido + o valor anterior
+    delay(10); //Delay se 10 milissegundos
   }
   ldr /= 10; //Divide o valor armazenado por 10
   Serial.println(ldr); //Imprime o valor do LDR
 
   (ldr >= 400) ? (digitalWrite(SAIDA_LDR,HIGH)) : (digitalWrite(SAIDA_LDR,LOW));
-  //Se o valor lido (luminosidade) for maior ou igual a 400, liga a lâmpada, senão desliga a lâmpada
+  //Se o valor lido (luminosidade) for maior ou igual a 400, ligo a saída, se não desligo
   delay(500);
 }
